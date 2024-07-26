b4b85535f9917a81884443c55af985ecdc20f110
iremcorak <iremcorak@gmail.com>
2024-07-26 14:10:54 +0300

    move labeled images method added

--- main.py
+++ main.py
@@ -10,3 +10,32 @@ def check_xml_name(xml_path):
         if name is not None and name.text.strip():
             return True
     return False
+
+def move_labeled_images(source_folder, destination_folder, unlabeled_folder):
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            if file.lower().endswith((".png", ".jpg")):
+                image_path = os.path.join(root, file)
+                xml_path = os.path.join(root, file.rsplit(".", 1)[0] + ".xml")
+
+                if os.path.exists(xml_path):
+                    if check_xml_name(xml_path):
+                        # Eğer XML dosyası varsa ve name etiketi doluysa, resmi ve etiket dosyasını hedef klasöre taşı
+                        destination_image_path = os.path.join(destination_folder, file)
+                        destination_xml_path = os.path.join(destination_folder, file.rsplit(".", 1)[0] + ".xml")
+
+                        shutil.move(image_path, destination_image_path)
+                        shutil.move(xml_path, destination_xml_path)
+
+                        print(f"{file} adlı resim ve etiket dosyası etiketli klasöre taşındı.")
+                    else:
+                        # Eğer XML dosyası var ama name etiketi boşsa, resmi ve XML'i etiketsiz klasöre taşı
+                        unlabeled_image_path = os.path.join(unlabeled_folder, file)
+                        unlabeled_xml_path = os.path.join(unlabeled_folder, file.rsplit(".", 1)[0] + ".xml")
+
+                        shutil.move(image_path, unlabeled_image_path)
+                        shutil.move(xml_path, unlabeled_xml_path)
+
+                        print(f"{file} adlı resim ve etiket dosyası etiketsiz klasöre taşındı.")
+                else:
+                    print(f"{file} adlı resmin etiket dosyası bulunamadı.")
